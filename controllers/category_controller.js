3d0a6b0aa3026b82f3bdb4c9b3e25d66895a5fb0
ahmedashour28 <66414402+ahmedashour28@users.noreply.github.com>
2023-03-31 02:03:52 +0200

    Update category_controller.js

--- controllers/category_controller.js
+++ controllers/category_controller.js
@@ -44,8 +44,14 @@ const editcategory = (req, res) => {
     if (!category)
         res.status(404).send('The category with the given ID not found !');
 
-    const result = validatecategory(req.body);
+    if(!req.body.name || req.body.name.length<3){
+       res.status(400).send('Name is required and must be more than 3 characters');
+       }
 
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    const result = schema.validate(req.body);
     if (result.error) {
         res.status(400).send(result.error.details[0].message);
         console.log(result);
