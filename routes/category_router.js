6992eda2b3fc9b5685a21ce52c7fec3ffe39f731
Yassmin-gamal <66153260+Yassmin-gamal@users.noreply.github.com>
2023-03-31 02:50:24 +0200

    Create category_router.js

---
+++ routes/category_router.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const categoryRouter = Router()
+
+const categoryController = require('../controllers/category_controller');
+
+categoryRouter.get('/', categoryController.getAllcategories);
+categoryRouter.get('/:name', categoryController.getcategoryByName);
+categoryRouter.post('/', categoryController.addcategory);
+categoryRouter.put('/:id', categoryController.editcategory);
+categoryRouter.delete('/:id', categoryController.deletecategory);
+
+module.exports = {
+    categoryRouter
+};
