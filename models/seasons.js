58ef11a4ee49e008b240e38f896d351585573a71
YaraKhalidM <66218339+YaraKhalidM@users.noreply.github.com>
2023-04-12 02:46:00 +0200

    Add files via upload

---
+++ models/seasons.js
@@ -0,0 +1,10 @@
+const seasons = [
+    { id: 1, name: "akkl" , episode: "jbjk" },
+    { id: 2, name: "akkl" , episode: "jbhjb" },
+    { id: 3, name: "jljnl" , episode: "kgjb" }
+    
+];
+
+module.exports = {
+    seasons,
+};
