57d7a6d6b59a75d7893a27ce9c523687e9b17f51
ahmedashour28 <66414402+ahmedashour28@users.noreply.github.com>
2023-04-13 23:59:14 +0200

    Update category_controller.js

--- controllers/category_controller.js
+++ controllers/category_controller.js
@@ -2,77 +2,60 @@ const { categories } = require('../models/categories');
 const Joi = require('joi');
 const { validatecategory } = require('../helper/validation');
 
-const getAllcategories = (req, res) => {
-    res.send(categories);
-};
-
-
-const getcategoryByName = (req, res) => {
-    const category = categories.find(c => c.name === req.params.name);
-    if (!category) {
-        res.status(404).send('The category with the given ID not found !');
-    } else {
-        res.send(category);
+const getAllcategories = async (req, res) => {
+    try {
+        const allcategories = await categories.find({});
+        res.status(200).send(allcategories);
+    } catch (error){
+        res.status(400).send(error);
     }
 };
 
-const addcategory = (req, res) => {
-      if(!req.body.name || req.body.name.length<3){
-       res.status(400).send('Name is required and must be more than 3 characters');
-       }
 
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-    const result = schema.validate(req.body);
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        console.log(result);
-        return;
+const getcategoryByName = async (req, res) => {
+    try {
+        const category = await categories.find({name:req.params.name});
+        res.status(200).send(category);
+    } catch (error){
+        res.status(400).send(error);
     }
-
-    const category = {
-        id: categories.length + 1,
-        name: req.body.name
-    };
-    categories.push(category);
-    res.send(category);
 };
 
-const editcategory = (req, res) => {
-    const category = categories.find(c => c.id === parseInt(req.params.id));
-    if (!category)
-        res.status(404).send('The category with the given ID not found !');
-
-    if(!req.body.name || req.body.name.length<3){
-       res.status(400).send('Name is required and must be more than 3 characters');
-       }
-
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-    const result = schema.validate(req.body);
-    if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        console.log(result);
-        return;
+const addcategory = async (req, res) => {
+    try {
+        const category = await categories.create(req.body);
+        res.status(200).send(category);
+    } catch (error){
+        res.status(400).send(error);
     }
+};
 
-    category.name = req.body.name;
-    res.send(category);
+const editcategory = async (req, res) => {
+    try {
+        const category = await categories.find({name:req.params.name});
+        if (!category){
+            return res.status(404).send('category not found');
+        }
+        if(req.body.name){
+            category.name = req.body.name;
+        }
+        await category.save();
+        res.status(200).send(category);
+    } catch (error){
+        res.status(400).send(error);
+    }
 
 };
 
 
-const deletecategory = (req, res) => {
-    const category = categories.find(c => c.id === parseInt(req.params.id));
-    if (!category)
-        res.status(404).send('The category with the given ID not found !');
 
-    const index = categories.indexOf(category);
-    categories.splice(index, 1);
-
-    res.send(category);
+const deletecategory = async (req, res) => {
+    try {
+        const category = await categories.deleteOne({name:req.params.name});
+        res.status(200).send(category);
+    } catch (error){
+        res.status(400).send(error);
+    }
 };
 
 module.exports = {
