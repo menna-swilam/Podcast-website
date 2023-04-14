9dc1a8459304af099822e5933416317be3d6c0e8
Yassmin-gamal <66153260+Yassmin-gamal@users.noreply.github.com>
2023-04-14 14:20:58 +0200

    Update podcaster_router.js

--- routes/podcaster_router.js
+++ routes/podcaster_router.js
@@ -5,7 +5,8 @@ const podcasterController = require('../controllers/podcaster_controller');
 
 
 podcasterRouter.get('/', podcasterController.getAllPodcasters);
-podcasterRouter.get('/:name', podcasterController.getPodcasterByName);
+podcasterRouter.get('/:id', podcasterController.getPodcasterById);
+podcasterRouter.get('/name/:name', podcasterController.getPodcasterByName);
 podcasterRouter.post('/', podcasterController.addPodcaster);
 podcasterRouter.put('/:id', podcasterController.editPodcaster);
 podcasterRouter.delete('/:id', podcasterController.deletePodcaster);
@@ -13,6 +14,3 @@ podcasterRouter.delete('/:id', podcasterController.deletePodcaster);
 module.exports = {
     podcasterRouter
 };
-
-
- 
